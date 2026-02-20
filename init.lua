eb8b77e65edf5b64e64e29f2960398bdbec81242
Marcel Richter <mail@mrclrchtr.de>
2026-02-20 12:52:42 +0100

    fix: nvim-treesitter/nvim-treesitter

--- init.lua
+++ init.lua
@@ -851,8 +851,10 @@ require('lazy').setup({
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ":TSUpdate",
     config = function()
-      local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      local filetypes = { 'bash', 'zsh', 'javascript', 'typescript', 'python', 'go', 'java', 'kotlin', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
       require('nvim-treesitter').install(filetypes)
       vim.api.nvim_create_autocmd('FileType', {
         pattern = filetypes,
